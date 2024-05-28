3daeca9f6b67eb90aba0b72d4d1f91eb0a404550
19dolly-hub <dollyraghuwanshi16@gmail.com>
2024-05-28 11:58:05 +0530

    font-face and favicon

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-const tl = gsap.timeline();
+const loaderAnim = () => {
+  const tl = gsap.timeline();
 
-tl.from("#loader p", {
-  y: 150,
-  stagger: 0.3,
-  delay: 0.5,
-  duration: 0.5,
-});
+  tl.from("#loader p", {
+    y: 150,
+    stagger: 0.3,
+    delay: 0.5,
+    duration: 0.5,
+  });
 
-tl.from("#counter, #now", {
-  opacity: 0,
-  onStart: function () {
-    const counter = document.querySelector("#counter span");
-    let count = 0;
+  tl.from("#counter, #now", {
+    opacity: 0,
+    onStart: function () {
+      const counter = document.querySelector("#counter span");
+      let count = 0;
 
-    const loadCounter = setInterval(function () {
-      if (count < 100) {
-        counter.innerHTML = count++;
-      } else {
-        counter.innerHTML = count;
-        clearInterval(loadCounter);
-      }
-    }, 35);
-  },
-});
+      const loadCounter = setInterval(function () {
+        if (count < 100) {
+          counter.innerHTML = count++;
+        } else {
+          counter.innerHTML = count;
+          clearInterval(loadCounter);
+        }
+      }, 35);
+    },
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.4,
-  delay: 4,
-});
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4,
+  });
 
-tl.from("#page1", {
-  delay: 0.2,
-  y: 1600,
-  opacity: 0,
-  ease: Power4,
-});
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+  });
 
-tl.to("#loader", {
-  display: "none",
-});
+  tl.to("#loader", {
+    display: "none",
+  });
+};
+
+loaderAnim();
